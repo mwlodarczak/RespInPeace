29df22a8ab7cc9ac8ae12ffb2153f3e7420d513b
Marcin Włodarczak <marcin@trurl>
2018-11-02 14:49:58 +0100

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# RespInPeace - Process and analyse breathing belt (RIP) data.
+# Copyright (C) 2018 Marcin Włodarczak
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from distutils.core import setup
+
+setup(
+    name='rip',
+    description='RespInPeace - Process and analyse breathing belt (RIP) data.',
+    version='0.9',
+    modules=['rip'],
+    maintainer='Marcin Włodarczak',
+    maintainer_email='wlodarczak@ling.su.se',
+    license='GNU General Public License 3',
+    download_url='https://github.com/mwlodarczak/RespInPeace/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+)
