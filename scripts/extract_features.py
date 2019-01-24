d8704ba6c485032af7af8290c13d6cfe54a81c48
Marcin Włodarczak <marcin@trurl>
2019-01-24 10:47:04 +0100

    Update extract_features.py.

--- scripts/extract_features.py
+++ scripts/extract_features.py
@@ -2,18 +2,26 @@
 
 import csv
 import os
-import sys
 
+import tgt
 import rip
 
 
-def main(wav_path, outfile_path):
+def main(wav_path, outfile_path, speech_path=None,
+         speech_tier_name=None):
+
+    if speech_path is not None and speech_path is not None:
+        tg = tgt.read_textgrid(speech_path)
+        speech = tg.get_tier_by_name(speech_tier_name)
+    else:
+        speech = None
 
     # Read the respiratory signal, detrend it, identify cycles and
     # holds.
-    resp = rip.RIP.from_wav(wav_path)
+    resp = rip.RIP.from_wav(wav_path, speech=speech)
     resp.remove_baseline()
     resp.find_cycles(include_holds=True)
+    resp.find_holds()
     resp.estimate_range()
     resp.estimate_rel(30)
     resp.save_annotations('breath.TextGrid')
@@ -24,23 +32,43 @@ def main(wav_path, outfile_path):
     # cycles.
 
     features = []
-    for i, (start, end) in enumerate(resp.segments):
-        features_seg = {'file': fname, 'start': start, 'end': end}
+    for i, seg in enumerate(resp.segments):
+
+        start = seg.start_time
+        end = seg.end_time
+        label = seg.text
+
+        features_seg = {'file': fname, 'start': start, 'end': end,
+                        'segment': label}
         # Odd-numbered rows correspond to inhalations and even-numbered
         # rows correspond to exhalations.
-        if i % 2:
-            prev_inh = resp.segments[i - 1,:]
-            features_seg['duty_cycle'] = (end - start) / (end - prev_inh[0])
-            features_seg['segment'] = 'exhalation'
+        if label == 'out':
+            cycle_start = resp.segments[i - 1].start_time
+            features_seg['duty_cycle'] = (end - start) / (end - cycle_start)
         else:
-            next_exh = resp.segments[i + 1,:]
-            features_seg['duty_cycle'] = (end - start) / (next_exh[1] - start)
-            features_seg['segment'] = 'inhalation'
+            cycle_end = resp.segments[i + 1].end_time
+            features_seg['duty_cycle'] = (end - start) / (cycle_end - start)
         features_seg['duration'] = end - start
         features_seg['slope'] = resp.extract_slope(start, end)
         features_seg['amplitude'] = resp.extract_amplitude(start, end)
         features_seg['vol_start'] = resp.extract_level(start)
         features_seg['vol_end'] = resp.extract_level(end)
+        holds = resp.holds.get_annotations_between_timepoints(
+            start, end, left_overlap=True, right_overlap=True)
+        features_seg['nholds'] = len(holds)
+
+        if len(holds):
+            holds_dur = sum(h.end_time - h.start_time for h in holds)
+            holds_dur -= max(0, start - holds[0].start_time)
+            holds_dur -= max(0, holds[-1].end_time - end)
+        else:
+            holds_dur = 0
+
+        if speech is not None:
+
+            # Extract: time lag, interval before and after: duration,
+            # start, end level, slope
+            pass
 
         features.append(features_seg)
 
